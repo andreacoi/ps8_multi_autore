c42f217841a4a60c41869a93c15842ed50a9b9a4
Andrea Coi <andrea.coi@outlook.it>
2024-06-18 11:34:13 +0200

    crea entry point per modulo

---
+++ authorsmanager.php
@@ -0,0 +1,59 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+  exit;
+}
+
+class AuthorsManager extends Module
+{
+  public function __construct()
+  {
+    $this->name = 'authorsmanager';
+    $this->version = '1.0.0';
+    $this->author = 'Andrea Coi';
+    $this->tab = 'administration';
+    $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    $this->bootstrap = true;
+
+    parent::__construct();
+
+    $this->displayName = $this->l('Gestisci autori');
+    $this->description = $this->l('Modulo per la gestione di autori multipli.');
+  }
+
+  public function install()
+  {
+    return parent::install() &&
+      $this->registerHook('displayAdminProductsExtra') &&
+      $this->installTab();
+  }
+
+  public function uninstall()
+  {
+    return parent::uninstall() && $this->uninstallTab();
+  }
+
+  private function installTab()
+  {
+    $tab = new Tab();
+    $tab->active = 1;
+    $tab->class_name = 'AdminAuthors';
+    $tab->name = array();
+    foreach (Language::getLanguages(true) as $lang) {
+      $tab->name[$lang['id_lang']] = 'Authors';
+    }
+    $tab->id_parent = (int)Tab::getIdFromClassName('AdminCatalog');
+    $tab->module = $this->name;
+
+    return $tab->add();
+  }
+
+  private function uninstallTab()
+  {
+    $id_tab = (int)Tab::getIdFromClassName('AdminAuthors');
+    if ($id_tab) {
+      $tab = new Tab($id_tab);
+      return $tab->delete();
+    }
+    return false;
+  }
+}
