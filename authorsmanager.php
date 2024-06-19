ce48608a1d9fabc33ec361cc44739643d55d879d
Andrea Coi <andrea.coi@outlook.it>
2024-06-19 12:14:10 +0200

    aggiornato file per l'installazione del modulo
    aggiorna versione di rilascio

--- authorsmanager.php
+++ authorsmanager.php
@@ -8,30 +8,16 @@ class AuthorsManager extends Module
   public function __construct()
   {
     $this->name = 'authorsmanager';
-    $this->version = '1.0.0';
-    $this->author = 'Andrea Coi';
     $this->tab = 'administration';
-    $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
-    $this->bootstrap = true;
+    $this->version = '0.0.1.5-pre-alpha';
+    $this->author = 'Andrea Coi';
+    $this->need_instance = 0;
 
     parent::__construct();
 
     $this->displayName = $this->l('Gestisci autori');
-    $this->description = $this->l('Modulo per la gestione di autori multipli.');
+    $this->description = $this->l('Modulo per gestire gli autori multipli per ogni libro.');
   }
-
-  public function install()
-  {
-    return parent::install() &&
-      $this->registerHook('displayAdminProductsExtra') &&
-      $this->installTab();
-  }
-
-  public function uninstall()
-  {
-    return parent::uninstall() && $this->uninstallTab();
-  }
-
   private function installTab()
   {
     $tab = new Tab();
@@ -39,7 +25,7 @@ class AuthorsManager extends Module
     $tab->class_name = 'AdminAuthors';
     $tab->name = array();
     foreach (Language::getLanguages(true) as $lang) {
-      $tab->name[$lang['id_lang']] = 'Authors';
+      $tab->name[$lang['id_lang']] = 'Autori';
     }
     $tab->id_parent = (int)Tab::getIdFromClassName('AdminCatalog');
     $tab->module = $this->name;
@@ -56,17 +42,54 @@ class AuthorsManager extends Module
     }
     return false;
   }
+  public function install()
+  {
+    return parent::install() &&
+      $this->registerHook('displayAdminProductsExtra') &&
+      $this->registerHook('actionAdminProductsControllerSaveAfter') &&
+      $this->createTables() &&
+      $this->installTab();
+  }
+
+  public function uninstall()
+  {
+    return parent::uninstall() && $this->deleteTables() && $this->uninstallTab();
+  }
+
+  protected function createTables()
+  {
+    $sql = "CREATE TABLE IF NOT EXISTS `" . _DB_PREFIX_ . "author` (
+            `id_author` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+            `first_name` VARCHAR(255) NOT NULL,
+            `last_name` VARCHAR(255) NOT NULL,
+            `biography` TEXT,
+            PRIMARY KEY (`id_author`)
+        ) ENGINE=" . _MYSQL_ENGINE_ . " DEFAULT CHARSET=utf8;";
+
+    $sql .= "CREATE TABLE IF NOT EXISTS `" . _DB_PREFIX_ . "product_author` (
+            `id_product` INT UNSIGNED NOT NULL,
+            `id_author` INT UNSIGNED NOT NULL,
+            `contribution_type` VARCHAR(255) NOT NULL,
+            PRIMARY KEY (`id_product`, `id_author`),
+            CONSTRAINT `fk_product_author_product` FOREIGN KEY (`id_product`) REFERENCES `" . _DB_PREFIX_ . "product` (`id_product`) ON DELETE CASCADE,
+            CONSTRAINT `fk_product_author_author` FOREIGN KEY (`id_author`) REFERENCES `" . _DB_PREFIX_ . "author` (`id_author`) ON DELETE CASCADE
+        ) ENGINE=" . _MYSQL_ENGINE_ . " DEFAULT CHARSET=utf8;";
+
+    return Db::getInstance()->execute($sql);
+  }
+
+  protected function deleteTables()
+  {
+    $sql = "DROP TABLE IF EXISTS `" . _DB_PREFIX_ . "product_author`;";
+    $sql .= "DROP TABLE IF EXISTS `" . _DB_PREFIX_ . "author`;";
+    return Db::getInstance()->execute($sql);
+  }
 
   public function hookDisplayAdminProductsExtra($params)
   {
     $id_product = (int)$params['id_product'];
     $authors = $this->getProductAuthors($id_product);
     $all_authors = $this->getAllAuthors();
-
-    // Debug output
-    error_log(print_r($params, true));
-    error_log(print_r($authors, true));
-    error_log(print_r($all_authors, true));
 
     $this->context->smarty->assign([
       'authors' => $authors,
@@ -75,23 +98,44 @@ class AuthorsManager extends Module
 
     return $this->display(__FILE__, 'views/templates/admin/products/helpers/form/form.tpl');
   }
-  private function getProductAuthors($id_product)
+
+  public function hookActionAdminProductsControllerSaveAfter($params)
   {
-    $sql = new DbQuery();
-    $sql->select('a.id_author, a.first_name, a.last_name, pa.contribution_type')
-      ->from('product_author', 'pa')
-      ->leftJoin('author', 'a', 'pa.id_author = a.id_author')
-      ->where('pa.id_product = ' . (int)$id_product);
+    $id_product = (int)$params['id_product'];
+    $authors = Tools::getValue('authors', []);
+    $contribution_types = Tools::getValue('contribution_types', []);
+
+    Db::getInstance()->execute('DELETE FROM ' . _DB_PREFIX_ . 'product_author WHERE id_product = ' . (int)$id_product);
+
+    if (!empty($authors)) {
+      foreach ($authors as $key => $id_author) {
+        Db::getInstance()->insert('product_author', [
+          'id_product' => (int)$id_product,
+          'id_author' => (int)$id_author,
+          'contribution_type' => pSQL($contribution_types[$key]),
+        ]);
+      }
+    }
+  }
+
+  protected function getProductAuthors($id_product)
+  {
+    if (!$id_product) {
+      return [];
+    }
+
+    $sql = '
+            SELECT pa.id_author, a.first_name, a.last_name, pa.contribution_type
+            FROM ' . _DB_PREFIX_ . 'product_author pa
+            LEFT JOIN ' . _DB_PREFIX_ . 'author a ON pa.id_author = a.id_author
+            WHERE pa.id_product = ' . (int)$id_product;
 
     return Db::getInstance()->executeS($sql);
   }
 
-  private function getAllAuthors()
+  protected function getAllAuthors()
   {
-    $sql = new DbQuery();
-    $sql->select('a.id_author, a.first_name, a.last_name')
-      ->from('author', 'a');
-
+    $sql = 'SELECT * FROM ' . _DB_PREFIX_ . 'author';
     return Db::getInstance()->executeS($sql);
   }
 }
