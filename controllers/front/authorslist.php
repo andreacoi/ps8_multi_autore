af6296cb3e90cc00bb9bbfc6aa6fb2769b77d485
Andrea Coi <andrea.coi@outlook.it>
2024-06-24 15:24:53 +0200

    ordina gli autori ricavati dal database per ordine alfabetico crescente

--- controllers/front/authorslist.php
+++ controllers/front/authorslist.php
@@ -7,9 +7,14 @@ class AuthorsManagerAuthorsListModuleFrontController extends ModuleFrontControll
 
     // Recupera la lista di tutti gli autori
     $authors = $this->getAllAuthors();
+    $letters = [
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ];
 
     $this->context->smarty->assign([
       'authors' => $authors,
+      'letters' => $letters,
     ]);
 
     $this->setTemplate('module:authorsmanager/views/templates/front/authors_list.tpl');
@@ -17,7 +22,7 @@ class AuthorsManagerAuthorsListModuleFrontController extends ModuleFrontControll
 
   private function getAllAuthors()
   {
-    $sql = 'SELECT * FROM ' . _DB_PREFIX_ . 'author';
+    $sql = 'SELECT * FROM ' . _DB_PREFIX_ . 'author ORDER BY last_name ASC';
     return Db::getInstance()->executeS($sql);
   }
 }
