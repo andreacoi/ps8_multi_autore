7af98a1cb0f28fa62ba164457575b3906bb06f4c
Andrea Coi <andrea@localhost.localdomain>
2024-06-30 12:57:32 +0200

    implementa vista singolo libro

--- controllers/front/authordetails.php
+++ controllers/front/authordetails.php
@@ -6,31 +6,46 @@ class AuthorsManagerAuthorDetailsModuleFrontController extends ModuleFrontContro
   {
     parent::initContent();
 
-    // Recupera l'ID dell'autore dalla richiesta
+    // Recupera l'ID dell'autore dalla URL
     $id_author = (int)Tools::getValue('id_author');
-    $author = $this->getAuthorById($id_author);
-    $books = $this->getBooksByAuthorId($id_author);
 
+    // Recupera i dettagli dell'autore dal database
+    $author = Db::getInstance()->getRow('SELECT * FROM ' . _DB_PREFIX_ . 'author WHERE id_author = ' . $id_author);
+
+    if (!$author) {
+      Tools::redirect('index.php?controller=404');
+    }
+
+    // Recupera i libri associati all'autore
+    $sql = 'SELECT p.id_product
+                FROM ' . _DB_PREFIX_ . 'product p
+                LEFT JOIN ' . _DB_PREFIX_ . 'product_author ab ON p.id_product = ab.id_product
+                WHERE ab.id_author = ' . (int)$id_author;
+    $result = Db::getInstance()->executeS($sql);
+
+    // Crea un array di prodotti
+    $products = [];
+    foreach ($result as $row) {
+      $products[] = (int)$row['id_product'];
+    }
+
+    // Recupera i dettagli dei prodotti
+    $productsForTemplate = [];
+    foreach ($products as $id_product) {
+      $product = new Product($id_product, true, $this->context->language->id);
+      if (Validate::isLoadedObject($product)) {
+        $productsForTemplate[] = $product;
+      }
+    }
+
+    // Assegna i dati al template
     $this->context->smarty->assign([
       'author' => $author,
-      'books' => $books,
+      'products' => $productsForTemplate,
+      'homeSize' => Image::getSize(ImageType::getFormattedName('home')),
     ]);
 
+    // Imposta il template per la visualizzazione
     $this->setTemplate('module:authorsmanager/views/templates/front/author_details.tpl');
-  }
-
-  private function getAuthorById($id_author)
-  {
-    $sql = 'SELECT * FROM ' . _DB_PREFIX_ . 'author WHERE id_author = ' . (int)$id_author;
-    return Db::getInstance()->getRow($sql);
-  }
-
-  private function getBooksByAuthorId($id_author)
-  {
-    $sql = 'SELECT p.id_product, p.name 
-                FROM ' . _DB_PREFIX_ . 'product p
-                INNER JOIN ' . _DB_PREFIX_ . 'product_author pa ON p.id_product = pa.id_product
-                WHERE pa.id_author = ' . (int)$id_author;
-    return Db::getInstance()->executeS($sql);
   }
 }
--- views/templates/front/author_details.tpl
+++ views/templates/front/author_details.tpl
@@ -1,22 +1,19 @@
-
-{if $author}
+{extends file='page.tpl'}
+{block name='page_content'}
     <div class="author-details">
-        <h1>{$author.first_name} {$author.last_name}</h1>
-        <p>{$author.biography}</p>
-        
-        {if $books}
-            <h2>{l s='Books by this author' d='Modules.AuthorsManager'}</h2>
-            <ul>
-                {foreach from=$books item=book}
-                    <li>
-                        <a href="{$link->getProductLink($book.id_product)}">{$book.name}</a>
-                    </li>
-                {/foreach}
-            </ul>
-        {else}
-            <p>{l s='No books found for this author.' d='Modules.AuthorsManager'}</p>
-        {/if}
+        <h1 class="text-center">{$author.first_name} {$author.last_name}</h1>
+        <p class="text-center">{$author.biography}</p>
+
+        <hr />
+
+        <div class="author-books">
+            <h2 class="text-center">Libri di {$author.first_name} {$author.last_name}</h2>
+            {if $products}
+                {include file="catalog/_partials/products.tpl" products=$products}
+            {else}
+                <p class="text-center">Nessun libro disponibile.</p>
+            {/if}
+        </div>
     </div>
-{else}
-    <p>{l s='Author not found.' d='Modules.AuthorsManager'}</p>
-{/if}
+{/block}
+
